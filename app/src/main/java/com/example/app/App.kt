598ab8b6ce8a761e8818d7b4548684fc4965c874
KKansy <Krzysztof.Kansy@hemmersbach.com>
2021-07-16 11:49:55 +0200

    - initialize koin

---
+++ app/src/main/java/com/example/app/App.kt
@@ -0,0 +1,20 @@
+package com.example.app
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initializeKoin()
+    }
+
+    private fun initializeKoin() = startKoin {
+        androidContext(this@App)
+        androidLogger()
+    }
+}
